6bb7d18f6e33af017220edb4292c5ab2ecaac14a
luca <lucareccia@hotmail.it>
2018-12-31 14:28:05 +0100

    Ready for 1.8. Crash on start resolved
    The data retriever method didn’t manage to reach the cdv due to an HTTP
    redirect that wasn’t handled

--- app/src/main/java/it/drrek/fuelsort/model/DataImporterManager.java
+++ app/src/main/java/it/drrek/fuelsort/model/DataImporterManager.java
@@ -2,8 +2,8 @@ package it.drrek.fuelsort.model;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
@@ -17,15 +17,62 @@ public class DataImporterManager {
     public static final String POMPE_PATH = "http://www.sviluppoeconomico.gov.it/images/exportCSV/prezzo_alle_8.csv";
 
     public String retrieve(String... params) throws IOException{
-        String link = params[0];
-        URL url = new URL(link);
-        InputStream is = url.openConnection().getInputStream();
-        StringBuilder buffer = new StringBuilder();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            buffer.append(line).append("\n");
+        String url = params[0];
+        try {
+
+            URL obj = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+            conn.setReadTimeout(5000);
+            conn.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
+            conn.addRequestProperty("User-Agent", "Mozilla");
+            conn.addRequestProperty("Referer", "google.com");
+
+            System.out.println("Request URL ... " + url);
+
+            boolean redirect = false;
+
+            // normally, 3xx is redirect
+            int status = conn.getResponseCode();
+            if (status != HttpURLConnection.HTTP_OK) {
+                if (status == HttpURLConnection.HTTP_MOVED_TEMP
+                    || status == HttpURLConnection.HTTP_MOVED_PERM
+                        || status == HttpURLConnection.HTTP_SEE_OTHER)
+                redirect = true;
+            }
+
+            if (redirect) {
+
+                // get redirect url from "location" header field
+                String newUrl = conn.getHeaderField("Location");
+
+                // get the cookie if need, for login
+                String cookies = conn.getHeaderField("Set-Cookie");
+
+                // open the new connnection again
+                conn = (HttpURLConnection) new URL(newUrl).openConnection();
+                conn.setRequestProperty("Cookie", cookies);
+                conn.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
+                conn.addRequestProperty("User-Agent", "Mozilla");
+                conn.addRequestProperty("Referer", "google.com");
+
+            }
+
+            BufferedReader in = new BufferedReader(
+                                      new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuffer html = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                html.append(inputLine).append("\n");
+            }
+            in.close();
+
+            return html.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return buffer.toString();
+
+        return "errore";
     }
 }
